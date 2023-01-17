b3f0cc2f39af1cf69b9e9e742fc570c6a6275cb5
Tran Nam Anh <ubik6958@gmail.com>
2023-01-17 16:06:32 +0700

    update

--- intro.js
+++ intro.js
@@ -16,39 +16,38 @@ buttonActive.onclick = function(){
     var rhombusImg = document.querySelector('.rhombus-img')
     var mailActive = document.querySelector('.mail')
 
-    boxFlower.classList.toggle("active")
-    boxFlower2.classList.toggle("active")
-    boxFlower3.classList.toggle("active")
-    boxFlower4.classList.toggle("active")
-    boxFlower5.classList.toggle("active")
-    boxFlower6.classList.toggle("active")
-    circleActive.classList.toggle("active")
-    boxsliderimg1.classList.toggle("active")
-    catActive.classList.toggle("active")
-    numberActive.classList.toggle("active")
-    buttonDisplay.classList.toggle("active")
-    rhombus1.classList.toggle("active")
-    rhombus2.classList.toggle("active")
-    rhombusImg.classList.toggle("active")
-    mailActive.classList.toggle("active")
+    // boxFlower.classList.toggle("active")
+    // boxFlower2.classList.toggle("active")
+    // boxFlower3.classList.toggle("active")
+    // boxFlower4.classList.toggle("active")
+    // boxFlower5.classList.toggle("active")
+    // boxFlower6.classList.toggle("active")
+    // circleActive.classList.toggle("active")
+    // boxsliderimg1.classList.toggle("active")
+    // catActive.classList.toggle("active")
+    // numberActive.classList.toggle("active")
+    // buttonDisplay.classList.toggle("active")
+    // rhombus1.classList.toggle("active")
+    // rhombus2.classList.toggle("active")
+    // rhombusImg.classList.toggle("active")
+    // mailActive.classList.toggle("active")
 }
 
-var mail = document.querySelector(".mail")
+
+
 var slider3 = document.querySelector(".slider3")
 var closeSlider3 = document.querySelector(".fa-xmark")
-mail.onclick = function(){
-    slider3.classList.add("active")
-}
+
 closeSlider3.addEventListener('click', function(){
     slider3.classList.remove('active')
 })
-
-
+var mailActive = document.querySelector(".button")
 // ------audio------------
 var buttonSong = document.querySelector('.button')
 var mySong = document.getElementById("song")
-buttonSong.onclick= function(){
-    if(mySong.paused){
-        mySong.play()
-    }
+mailActive.onclick = function(){
+	slider3.classList.add("active");
+	if(mySong.paused){
+		mySong.play()
+	}
 }
